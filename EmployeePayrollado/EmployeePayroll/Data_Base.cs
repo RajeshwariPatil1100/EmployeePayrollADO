12d3f0958f4ced9d8103a04d572b4ffc68c7f88e
RajeshwariPatil1100 <rajeshwaripatil1100@gmail.com>
2023-01-19 14:20:25 +0530

    Employee Payroll ado

---
+++ EmployeePayrollado/EmployeePayroll/Data_Base.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollado.EmployeePayroll
+{
+    public class Data_Base
+    {
+        public void CreateDatabass()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-2LOKNJ2\SQLEXPRESS;Initial catalog=master;Integrated Security=true");
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("Create database Employee_Payroll", connection);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Databass ctearted");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
